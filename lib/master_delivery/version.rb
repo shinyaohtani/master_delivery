211cb5f0449d2064e98c9abc1ed3de8e1bfbdbaa
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2020-04-28 12:02:18 +0900

    updated description about defference with rsync

--- lib/master_delivery/version.rb
+++ lib/master_delivery/version.rb
@@ -7,5 +7,18 @@ module MasterDelivery
     into the specified directory while maintaining the hierarchy of the
     master snapshot directory. If the destination file already exists,
     back it up first and then deliver the master file.
+
+    The difference with rsync is that master_delivery creates a symlinks
+    instead of copying the master files. They are symlinks, so you have to
+    keep in mind that you have to keep the master files in the same location,
+    but it also has the advantage that the master file is updated at the same
+    time when you directly make changes to the delivered file.
+
+    Do you have any experience that the master file is getting old gradually?
+    master_delivery can prevent this.
+
+    If the master directory is git or svn managed, you can manage revisions
+    of files that are delivered here and there at once with commands
+    like git diff and git commit.
   DESC
 end
